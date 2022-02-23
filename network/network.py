960cc6ed5d0063b7ff796d4ddb743ab1452dc2a6
komo <66017773+nagikomo@users.noreply.github.com>
2022-02-23 13:51:02 +0900

    Update network.py

--- network/network.py
+++ network/network.py
@@ -854,84 +854,55 @@ class Model:
         return SAMModel(inputs, x) if self.sam else tf.keras.Model(inputs, x)
 
 
-# efficientnet
-efficientnet_b0 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 0, efficientv1=True)
-efficientnet_b1 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 1, efficientv1=True)
-efficientnet_b2 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 2, efficientv1=True)
-efficientnet_b3 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 3, efficientv1=True)
-efficientnet_b4 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 4, efficientv1=True)
-efficientnet_b5 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 5, efficientv1=True)
-efficientnet_b6 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 6, efficientv1=True)
-efficientnet_b7 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 7, efficientv1=True)
+network_dict = {}
 
-sam_efficientnet_b0 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 0, efficientv1=True, sam=True)
-sam_efficientnet_b1 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 1, efficientv1=True, sam=True)
-sam_efficientnet_b2 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 2, efficientv1=True, sam=True)
-sam_efficientnet_b3 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 3, efficientv1=True, sam=True)
-sam_efficientnet_b4 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 4, efficientv1=True, sam=True)
-sam_efficientnet_b7 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 7, efficientv1=True, sam=True)
 
-dense_efficientnet_b0 = lambda: Model([], 0, "DepthwiseConv1D", "densenet", 0, efficientv1=True)
+def create_network(name, num_layer, dim, layer_name, types, **kwargs):
+    for i in range(8):
+        network_dict.update(
+            {f"{name}_b{i}": lambda: Model(num_layer, dim, layer_name, types, i, **kwargs)})
 
-lambda_efficientnet_b0 = lambda: Model([], 0, "lambdalayer", "resnet", 0, efficientv1=True)
-lambda_efficientnet_b1 = lambda: Model([], 0, "lambdalayer", "resnet", 1, efficientv1=True)
-lambda_efficientnet_b4 = lambda: Model([], 0, "lambdalayer", "resnet", 4, efficientv1=True)
-lambda_efficientnet_b6 = lambda: Model([], 0, "lambdalayer", "resnet", 6, efficientv1=True)
-lambda_efficientnet_b7 = lambda: Model([], 0, "lambdalayer", "resnet", 7, efficientv1=True)
 
-sam_lambda_efficientnet_b0 = lambda: Model([], 0, "lambdalayer", "resnet", 0, efficientv1=True, sam=True)
-sam_lambda_efficientnet_b2 = lambda: Model([], 0, "lambdalayer", "resnet", 2, efficientv1=True, sam=True)
-sam_lambda_efficientnet_b3 = lambda: Model([], 0, "lambdalayer", "resnet", 3, efficientv1=True, sam=True)
-sam_lambda_efficientnet_b4 = lambda: Model([], 0, "lambdalayer", "resnet", 4, efficientv1=True, sam=True)
+create_network("efficientnet", [], 0, "DepthwiseConv1D", "resnet", efficientv1=True)
+create_network("sam_efficientnet", [], 0, "DepthwiseConv1D", "resnet", efficientv1=True, sam=True)
 
-# efficientnetv2
-efficientnetv2_b0 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 0, efficientv2=True)
-efficientnetv2_b2 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 2, efficientv2=True)
-efficientnetv2_b4 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 4, efficientv2=True)
-efficientnetv2_b7 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 7, efficientv2=True)
+create_network("dense_efficientnet", [], 0, "DepthwiseConv1D", "densenet", efficientv1=True)
+create_network("sam_dense_efficientnet", [], 0, "DepthwiseConv1D", "densenet", efficientv1=True, sam=True)
 
-sam_efficientnetv2_b2 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 2, efficientv2=True, sam=True)
-sam_efficientnetv2_b4 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 4, efficientv2=True, sam=True)
-sam_efficientnetv2_b7 = lambda: Model([], 0, "DepthwiseConv1D", "resnet", 7, efficientv2=True, sam=True)
+create_network("lambda_efficientnet", [], 0, "lambdalayer", "resnet", efficientv1=True)
+create_network("sam_lambda_efficientnet", [], 0, "lambdalayer", "resnet", efficientv1=True, sam=True)
 
-# resnet
-se_lambda_resnet_b0 = lambda: Model([], 48, "LambdaLayer", "resnet", 0, se=True)
-se_lambda_resnet_b1 = lambda: Model([], 48, "LambdaLayer", "resnet", 1, se=True)
+create_network("efficientnetv2", [], 0, "DepthwiseConv1D", "resnet", efficientv2=True)
+create_network("sam_efficientnetv2", [], 0, "DepthwiseConv1D", "resnet", efficientv2=True, sam=True)
 
-# convnext
-se_convnext_b0 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 0, convnext=True, se=True)
-se_convnext_b1 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 1, convnext=True, se=True)
-se_convnext_b4 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 4, convnext=True, se=True)
-se_convnext_b7 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 7, convnext=True, se=True)
+create_network("resnet", [], 48, "Conv1D", "resnet")
+create_network("sam_resnet", [], 48, "Conv1D", "resnet", sam=True)
+create_network("se_resnet", [], 48, "Conv1D", "resnet", se=True)
+create_network("sam_se_resnet", [], 48, "Conv1D", "resnet", se=True, sam=True)
 
-sam_se_convnext_b0 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 0, convnext=True, se=True, sam=True)
-sam_se_convnext_b2 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 2, convnext=True, se=True, sam=True)
-sam_se_convnext_b4 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 4, convnext=True, se=True, sam=True)
-sam_se_convnext_b7 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 7, convnext=True, se=True, sam=True)
+create_network("densenet", [], 48, "Conv1D", "densenet")
+create_network("sam_densenet", [], 48, "Conv1D", "densenet", sam=True)
+create_network("se_densenet", [], 48, "Conv1D", "densenet", se=True)
+create_network("sam_se_densenet", [], 48, "Conv1D", "densenet", se=True, sam=True)
 
-cbam_convnext_b0 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 0, convnext=True, cbam=True)
-cbam_convnext_b2 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 2, convnext=True, cbam=True)
-cbam_convnext_b4 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 4, convnext=True, cbam=True)
-cbam_convnext_b7 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 7, convnext=True, cbam=True)
+create_network("lambda_resnet", [], 48, "LambdaLayer", "resnet")
+create_network("sam_lambda_resnet", [], 48, "LambdaLayer", "resnet", sam=True)
+create_network("se_lambda_resnet", [], 48, "LambdaLayer", "resnet", se=True)
+create_network("sam_se_lambda_resnet", [], 48, "LambdaLayer", "resnet", se=True, sam=True)
 
-sam_cbam_convnext_b2 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 2, convnext=True, cbam=True, sam=True)
-sam_cbam_convnext_b4 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 4, convnext=True, cbam=True, sam=True)
-sam_cbam_convnext_b7 = lambda: Model([], 48, "DepthwiseConv1D", "resnet", 7, convnext=True, cbam=True, sam=True)
+create_network("convnext", [], 48, "DepthwiseConv1D", "resnet", convnext=True)
+create_network("sam_convnext", [], 48, "DepthwiseConv1D", "resnet", convnext=True, sam=True)
+create_network("se_convnext", [], 48, "DepthwiseConv1D", "resnet", convnext=True, se=True)
+create_network("sam_se_convnext", [], 48, "DepthwiseConv1D", "resnet", convnext=True, se=True, sam=True)
 
-se_lambda_convnext_b0 = lambda: Model([], 48, "LambdaLayer", "resnet", 0, convnext=True, se=True)
-se_lambda_convnext_b1 = lambda: Model([], 48, "LambdaLayer", "resnet", 1, convnext=True, se=True)
-se_lambda_convnext_b4 = lambda: Model([], 48, "LambdaLayer", "resnet", 4, convnext=True, se=True)
-se_lambda_convnext_b7 = lambda: Model([], 48, "LambdaLayer", "resnet", 7, convnext=True, se=True)
-
-sam_se_lambda_convnext_b0 = lambda: Model([], 48, "LambdaLayer", "resnet", 0, convnext=True, se=True, sam=True)
-sam_se_lambda_convnext_b1 = lambda: Model([], 48, "LambdaLayer", "resnet", 1, convnext=True, se=True, sam=True)
-sam_se_lambda_convnext_b2 = lambda: Model([], 48, "LambdaLayer", "resnet", 2, convnext=True, se=True, sam=True)
-sam_se_lambda_convnext_b4 = lambda: Model([], 48, "LambdaLayer", "resnet", 4, convnext=True, se=True, sam=True)
-sam_se_lambda_convnext_b7 = lambda: Model([], 48, "LambdaLayer", "resnet", 7, convnext=True, se=True, sam=True)
+create_network("lambda_convnext", [], 48, "LambdaLayer", "resnet", convnext=True)
+create_network("sam_lambda_convnext", [], 48, "LambdaLayer", "resnet", convnext=True, sam=True)
+create_network("se_lambda_convnext", [], 48, "LambdaLayer", "resnet", convnext=True, se=True)
+create_network("sam_se_lambda_convnext", [], 48, "LambdaLayer", "resnet", convnext=True, se=True, sam=True)
 
 
 def build_model(model_name: str, input_shape: tuple, output_size: int) -> keras.Model:
-    model = eval(model_name)()
+    model = network_dict[model_name]
     print(noise_ratio)
     model = model.build_model(input_shape, output_size, )
 
